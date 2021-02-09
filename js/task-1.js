7daa2ef26d00c1de720d9c47dafdc10c56b9cdf0
Iryna Konoplova <a.irina773@gmail.com>
2021-02-10 01:20:43 +0200

    Решает задание 1

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+const productName = 'Генератор защитного поля';
+let price = 1000;
+let message = `Выбран "${productName}", цена за штуку ${price} кредитов`
+console.log(message);
+
+price = 2000;
+message = `Выбран "${productName}", цена за штуку ${price} кредитов`
+console.log(message);
